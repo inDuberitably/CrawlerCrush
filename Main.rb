309e9f0373215a2dc302bd0f02cbe80e27035d93
inDuberitably <duberstein.4@wright.edu>
2014-12-21 15:01:13 -0500

    Marshal now active,l oads session if it exists. Delete now removes active session from working directory.

--- Main.rb
+++ Main.rb
@@ -1,11 +1,18 @@
 require './CrushAnalysis.rb'
-
-CA = CrushAnalysis.new("WRIGHT_CRUSHES_asText.txt","WRIGHT_CRUSHES_asText.txt", "StopWords.txt")
+if !File.exist?('saved_session')
+  puts "Generating new session: "
+  CA = CrushAnalysis.new("WRIGHT_CRUSHES_asText.txt","WRIGHT_CRUSHES_asText.txt", "StopWords.txt")
+else
+  File.open('saved_session') do |f|
+    puts "saved_session loading..."
+    CA = Marshal.load(f)
+  end
+end
 print "\a"
-puts "Options:\nFind\nHTML\nPrint Tweets\nGo To\nLoad\nSave"
+puts "Press enter to begin:"
 keyboard = STDIN.gets.chomp
 while (true)
-  puts "Options:\nFind\nHTML\nPrint Tweets\nLoad\nSave"
+  puts "\sOptions:Find\tHTML\tReload Text\tPrint Tweets\tDelete\tSave"
   if (keyboard.include? "Find" or keyboard.include? "find")
     print "Enter the word you would like to find: "
     keyboard = STDIN.gets.chomp
@@ -15,13 +22,24 @@ while (true)
     puts "HTML generated"
   elsif keyboard.eql? "Print" or keyboard.eql? "print"
     CA.print_tweets
-  elsif keyboard.eql? "Load" or keyboard.eql? "load"
-
-    puts "unsupported operation"
-
+  elsif keyboard.eql? "Delete" or keyboard.eql? "delete"
+    File.delete("saved_session")
+    puts "saved_session was deleted."
   elsif keyboard.eql? "Save" or keyboard.eql? "save"
 
-    puts "unsupported operation"
+    File.open('saved_session', 'w+') do |f|
+      Marshal.dump(CA, f)
+    end
+
+  elsif keyboard.eql? "Reload" or keyboard.eql? "reload"
+    prints "File to read from: "
+    file=keyboard
+    puts file.length
+    print "Starting point"
+    starting =keyboard
+    print "Ending point"
+    ending = keyboard
+    CA.redefine_target_sector(file,starting,ending)
 
   elsif keyboard.eql? "go to" or keyboard.eql? "Go To"
     puts "Tweet or Word?"
@@ -36,7 +54,6 @@ while (true)
       puts "unsupported operation"
     end
 
-  else
-    keyboard = STDIN.gets.chomp
   end
+  keyboard = STDIN.gets.chomp
 end
